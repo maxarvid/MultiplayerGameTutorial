321f94188200dc78167e108c4450b44bca36cd27
Max <max.arvid.anderson@gmail.com>
2019-07-02 14:09:18 +0200

    player states

--- server.js
+++ server.js
@@ -29,3 +29,33 @@ io.on('connection', function(socket) {
 setInterval(function() {
   io.sockets.emit('message', 'hi!')
 }, 1000);
+
+// player keyboard input
+var players = {};
+io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
